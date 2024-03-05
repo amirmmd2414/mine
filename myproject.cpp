47416ced8cbf7938f0eed37e3c909169b5630423
amirmmd2414 <158852730+amirmmd2414@users.noreply.github.com>
2024-03-05 18:17:45 +0330

    Update myproject.cpp

--- myproject.cpp
+++ myproject.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
+void swap(int* a,int * b){
 
+}
 int main(){
-
+    int a = 1;
+    int b = 2;
+    swap(&a,&b);
     cout << "this does exist" << endl;
-
+    cout << a << endl << b << endl; 
     return 0;
 }
